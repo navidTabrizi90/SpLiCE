44f4adbc4064d300df7554ca21789bcd27d44a20
Navid Tabrizi <navidtabrizi90@gmail.com>
2025-12-11 10:59:47 +0100

    update requirments and init files

---
+++ splice/__init__.py
@@ -0,0 +1,6 @@
+# splice/__init__.py
+from .feature_extractor import CLIPFeatureExtractor
+from .dictionary import ConceptDictionary
+from .aligner import ModalityAligner
+from .solver import ADMMSpLICESolver
+from .interpreter import SpLiCEInterpreter
